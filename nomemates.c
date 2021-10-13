ce8d202af097b8d61543596fa70135ff45b4edb2
jessicavazquezz <a01639945@itesm.mx>
2021-10-13 03:06:50 +0000

    cambios

---
+++ nomemates.c
@@ -0,0 +1,18 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+void handleSignal(int signal){
+    printf("Jajaja no me matas\n");
+
+}
+
+int main(){
+    signal(2,handleSignal);
+    while(1){
+        printf("Trabajando \n");
+        sleep(1);
+    }
+    return 0;
+}
